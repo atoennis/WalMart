c6a483f20f0eabee0908b599eb094fd9ed18464d
Adam Toennis <adam@madebyatomicrobot.com>
2015-11-02 09:41:05 -0500

    More test coverage for the product presenter.

--- app/src/test/java/com/atoennis/walmartcodechallenge/presenters/ProductPresenterTest.java
+++ app/src/test/java/com/atoennis/walmartcodechallenge/presenters/ProductPresenterTest.java
@@ -2,11 +2,13 @@ package com.atoennis.walmartcodechallenge.presenters;
 
 
 import com.atoennis.walmartcodechallenge.data.ProductService;
+import com.atoennis.walmartcodechallenge.data.ProductService.GetProductsRequest;
 import com.atoennis.walmartcodechallenge.presenters.ProductPresenter.State;
 
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+import org.mockito.ArgumentCaptor;
 
 import static org.mockito.Mockito.*;
 
@@ -14,10 +16,11 @@ public class ProductPresenterTest {
 
     private ProductPresenter productPresenter;
     private State state;
+    private ProductService productService;
 
     @Before
     public void setup() {
-        ProductService productService = mock(ProductService.class);
+        productService = mock(ProductService.class);
         state = new State();
         productPresenter = new ProductPresenter(productService);
         productPresenter.state = state;
@@ -41,5 +44,31 @@ public class ProductPresenterTest {
 
         int nextPageNumber = productPresenter.generateNextPageNumber();
         Assert.assertEquals(1, nextPageNumber);
+    }
+
+    @Test
+    public void testHandleScrollToBottomOfPageWithNoMoreProducts() {
+        state.pageNumber = 1;
+        state.nextPageNumber = 1;
+
+        productPresenter.onScrolledToBottomOfList();
+
+        verify(productService, times(0)).getProducts(any(GetProductsRequest.class));
+    }
+
+    @Test
+    public void testHandleScrollToBottomOfPageWithMoreProducts() {
+        state.pageNumber = 1;
+        state.nextPageNumber = 2;
+        state.pageSize = 30;
+
+        productPresenter.onScrolledToBottomOfList();
+
+        ArgumentCaptor<GetProductsRequest> argumentCaptor = ArgumentCaptor.forClass(GetProductsRequest.class);
+        verify(productService, times(1)).getProducts(argumentCaptor.capture());
+
+        GetProductsRequest getProductsRequest = argumentCaptor.getValue();
+        Assert.assertEquals(1, getProductsRequest.pageNumber);
+        Assert.assertEquals(30, getProductsRequest.pageSize);
     }
 }
